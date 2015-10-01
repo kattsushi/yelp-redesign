1826ed0f492f309e242ad1c087be44f7fca2fb5a
Stephen Weber <me@stephenweber.io>
2015-10-01 06:14:44 -0700

    Gulp adjustment
    Gulp adjustment to make it easier for others to get started

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ gulp.task('bower_css', function () {
  * description: launches server
  */
 // Static Server + watching scss/html files
-gulp.task('serve', ['html','imagemin','js','sass'], function() {
+gulp.task('serve', ['html','imagemin','data','fonts','js','sass'], function() {
 
     browserSync.init({
         server: "./build"
